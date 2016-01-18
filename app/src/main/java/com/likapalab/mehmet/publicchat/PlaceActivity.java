39b82849da11a3f061471db3f4dbef2255ddfc52
Mehmet <ozdemirmehmt@gmail.com>
2016-01-19 01:54:07 +0200

    add intent transition animation

--- app/src/main/java/com/likapalab/mehmet/publicchat/PlaceActivity.java
+++ app/src/main/java/com/likapalab/mehmet/publicchat/PlaceActivity.java
@@ -254,10 +254,10 @@ public class PlaceActivity extends Activity implements View.OnClickListener {
                 myLocationName = Place.getPlaces().get(position).getName();
 
                 MainActivity.mTracker.send(new HitBuilders.EventBuilder()
-                        .setCategory("Selection")
-                        .setAction("Place")
-                        .setLabel(myLocationName)
-                        .build()
+                                .setCategory("Selection")
+                                .setAction("Place")
+                                .setLabel(myLocationName)
+                                .build()
                 );
 
                 Intent i = new Intent(getApplicationContext(),MessageActivity.class);
@@ -265,7 +265,6 @@ public class PlaceActivity extends Activity implements View.OnClickListener {
                 i.putExtra("location",myLocationName);
                 startActivity(i);
                 PlaceActivity.this.finish();
-                overridePendingTransition(R.anim.anim_left_in, R.anim.anim_left_out);
             }
         });
     }
